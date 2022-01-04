a04f76985f04a5a44d841df9d95c8a39e5a462c9
Jacobcar <92916001+Jacobcar@users.noreply.github.com>
2022-01-03 17:59:46 -0800

    initial upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// API key from https://home.openweathermap.org/api_keys
+var apiKey = "f5abdeb9818535349e07db956f98985c";
+var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey;
+
+
+fetch (requestUrl) {
+}
